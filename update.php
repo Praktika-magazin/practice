a927253bbae593afb05b12be922c90b225242484
MarusiaTitova <marina.marmelad777@gmail.com>
2021-07-12 01:44:13 +0300

    изменение для таблицы сотрудники

---
+++ update.php
@@ -0,0 +1,102 @@
+<?php 
+include 'connect.php'; 
+ //Если переменная Name передана
+ if (isset($_POST['fullname'])) { 
+ //Если это запрос на обновление, то обновляем
+ if (isset($_GET['red_id'])) { 
+ $sql_update = "UPDATE employee SET fullname = 
+'{$_POST['fullname']}', id_employee = '{$_POST['id_employee']}', date_of_birthday = 
+'{$_POST['date_of_birthday']}'
+id_post = '{$_POST['id_post']}' WHERE id_employee = 
+{$_GET['red_id']}"; 
+ $result_update = mysqli_query($link, $sql_update); 
+ } 
+ 
+ if (isset($_GET['red_id'])) { 
+     $sql_select = "SELECT fullname, id_employee, date_of_birthday, id_post from employee WHERE id_employee = {$_GET['red_id']}"; 
+     $result_select = mysqli_query($link, $sql_select); 
+ $row = mysqli_fetch_array($result_select); 
+ } 
+ ?>
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title> Редактирование </title>
+</head>
+
+<body>
+    <form action="" method="post">
+        <table>
+        <tr>
+                <td>Код сотрудника</td>
+                <td><input type="text" name="id_employee" value="<?= 
+isset($_GET['red_id']) ? $row['id_employee'] : ''; ?>"></td>
+            </tr>
+            <tr>
+            <tr>
+                <td>ФИО сотрудника</td>
+                <td><input type="text" name="fullname" value="<?= 
+isset($_GET['red_id']) ? $row['fullname'] : ''; ?>"></td>
+            </tr>
+
+                <td>Дата рождения</td>
+                <td><input type="text" name="date_of_birthday" value="<?= 
+isset($_GET['red_id']) ? $row['date_of_birthday'] : ''; ?>"></td>
+            </tr>
+
+                <td>Должность сотрудника</td>
+                <td>
+
+<!--
+                    <input type="text" name="id_post" value="<?= 
+isset($_GET['red_id']) ? $row['id_post'] : ''; ?>">
+-->
+
+
+                                               <select name="id_post">
+                        <?php
+                        
+                        
+//подключаемся к базе данных
+//include 'connect.php';
+// $inner = "SELECT employee.fio_employee, employee.id_employee, employee.date_of_birth, employee.phone_num, employee.id_post, employee.gender, employee.experience, post.id_post, post.post, post.zp, post.bonus FROM employee INNER JOIN post ON employee.id_post=post.id_post;";
+//$result_inner = mysqli_query ($link, $inner);
+//                        if ($result_inner) { 
+// echo '<p>Успешно!</p>';
+// } else { 
+// echo '<p>Произошла ошибка: ' . mysqli_error($link) 
+//. '</p>'; 
+// } 
+//$row_inner = mysqli_fetch_array($result_inner);
+//    echo    "<option>" "<input value=" .isset($_GET['red_id']) ? $row['id_post'] : '';" >". "</option>";
+
+//выполняем запрос, для получения данных из таблицы post
+$sql_select = "SELECT id_post, post FROM post";
+$result_select = mysqli_query($link, $sql_select);
+//циклом формируем значения, которые были получены в результате выполнения запроса
+echo    "<option value = ' ".$row['id_post']." '>".$row['id_post']."</option>";
+
+while ($row = mysqli_fetch_array($result_select))
+{
+//выводим данные из запроса в поле select
+echo
+   
+    "<option value = ' ".$row['id_post']." '>".$row['post']."</option>";
+// "<option value = ' ".$row['id_post']." '>".$row['post']."</option>";
+}
+?>
+                    </select>
+                </td>
+            </tr>
+
+            <tr>
+                <td colspan="2"><input type="submit" value="Сохранить"></td>
+            </tr>
+        </table>
+    </form>
+    <form action="header.php" method="post">
+        <input type="submit" value="Вернуться назад">
+    </form>
+</body>
+</html>
