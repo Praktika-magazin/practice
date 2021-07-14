fa4d27fd268c34d7e71d1a8d459dc52ea79b4854
leonovagela <angela-2-0-0-2@mail.ru>
2021-07-14 22:20:41 +0300

    ещё один хидер(чтобы при нажатии кнопки назад было всё ок)

---
+++ index_1.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Подключение БД к provider</title>
+</head>
+<body>
+<form action="index_employee_admin.php" method="post">
+        <input type="submit" name="connect" value="сотрудники">
+    </form>
+    <form action="index_post_admin.php" method="post">
+        <input type="submit" name="connect" value="должность">
+    </form>
+    <form action="index_department_admin.php" method="post">
+        <input type="submit" name="connect" value="отдел">
+    </form>
+    <form action="index_contract_admin.php" method="post">
+        <input type="submit" name="connect" value="Договор">
+    </form>
+</body>
+</html>
