75d6a6b0cddc362c23ecb4159a33f2d269adce86
MarusiaTitova <marina.marmelad777@gmail.com>
2021-07-16 01:06:31 +0300

    Update index_contract_admin.php

--- index_contract_admin.php
+++ index_contract_admin.php
@@ -128,22 +128,22 @@
         </tr>
         <?php 
      $sql_state = "SELECT contract.id_contract, employee.fullname, post.post, department.name, contract.date_of_issue, contract.education, contract.premium FROM contract INNER JOIN post ON contract.id_post=post.id_post 
-     INNER JOIN employee ON contract.id_employee = employee.id_employee INNER JOIN department ON contract.id_department =employee.id_post ";
-$result_state = mysqli_query($link, $sql_state);
-while ($row_state = mysqli_fetch_array($result_state)) { 
- echo '<tr>' .
- "<td> {$row_state['id_contract']}</td>".
-"<td> {$row_state['fullname']} </td>" .
-"<td> {$row_state['post']}</td>".
-"<td> {$row_state['name']}</td>".
-"<td> {$row_state['date_of_issue']}</td>".
-"<td> {$row_state['education']}</td>".
-"<td> {$row_state['premium']}</td>".
-"<td><a href='?del_id={$row_state['id_employee']}'>Удалить</a></td>".
-"<td><a href='update_cont.php?red_id={$row_state['id_employee']}'>Изменить</a></td>".
-'</tr>'; 
- } 
- ?>
+     INNER JOIN employee ON contract.id_employee = employee.id_employee INNER JOIN department ON contract.id_department = department.id_department ";
+        $result_state = mysqli_query($link, $sql_state);
+        while ($row_state = mysqli_fetch_array($result_state)) { 
+        echo '<tr>' .
+        "<td> {$row_state['id_contract']}</td>".
+        "<td> {$row_state['fullname']} </td>" .
+        "<td> {$row_state['post']}</td>".
+        "<td> {$row_state['name']}</td>".
+        "<td> {$row_state['date_of_issue']}</td>".
+        "<td> {$row_state['education']}</td>".
+        "<td> {$row_state['premium']}</td>".
+        "<td><a href='?del_id={$row_state['id_employee']}'>Удалить</a></td>".
+        "<td><a href='update_cont.php?red_id={$row_state['id_employee']}'>Изменить</a></td>".
+        '</tr>'; 
+        } 
+        ?>
     </table>
     <form action="header02.php" method="post">
         <input type="submit" value="Вернуться назад">
