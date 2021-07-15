3f0ce4963906e61d50fd5d6020abf7c82159d8b3
MarusiaTitova <marina.marmelad777@gmail.com>
2021-07-16 01:31:54 +0300

    Update update_cont.php

--- update_cont.php
+++ update_cont.php
@@ -3,20 +3,20 @@ include 'connect.php';
  //Если переменная Name передана
  if (isset($_POST['education'])) { 
  //Если это запрос на обновление, то обновляем
- if (isset($_GET['red_id'])) { 
- $sql_update = "UPDATE contract SET id_contract = '{$_POST['id_contract']}', id_employee = '{$_POST['id_employee']}', id_department = '{$_POST['id_department']}', id_post = '{$_POST['id_post']}' 
- date_of_issue = '{$_POST['date_of_issue']}', education = '{$_POST['education']}',premium = '{$_POST['premium']}' WHERE id_contract = {$_GET['red_id']}"; 
- $result_update = mysqli_query($link, $sql_update); 
- } 
- } 
+    if (isset($_GET['red_id'])) { 
+        $sql_update = "UPDATE contract SET id_contract = '{$_POST['id_contract']}', id_employee = '{$_POST['id_employee']}', id_department = '{$_POST['id_department']}', id_post = '{$_POST['id_post']}',
+        date_of_issue = '{$_POST['date_of_issue']}', education = '{$_POST['education']}',premium = '{$_POST['premium']}' WHERE id_contract = {$_GET['red_id']}"; 
+        $result_update = mysqli_query($link, $sql_update); 
+    } 
+    } 
  
  if (isset($_GET['red_id'])) { 
-     $sql_select = "SELECT id_contract , id_employee, id_department, id_post
-     date_of_issue, education, premium WHERE id_contract = {$_GET['red_id']}";
+     $sql_select = "SELECT id_contract, id_employee, id_department, id_post, date_of_issue, education, premium FROM contract WHERE id_contract = {$_GET['red_id']}";
      $result_select = mysqli_query($link, $sql_select); 
- $row = mysqli_fetch_array($result_select); 
+        $row = mysqli_fetch_array($result_select); 
  } 
  ?>
+ 
 <!DOCTYPE html>
 <html>
 
@@ -27,16 +27,17 @@ include 'connect.php';
 <body>
     <form action="" method="post">
         <table>
+        <tr>
+                <td>Код договора</td>
+                <td><input type="text" name="id_contract" value="<?= 
+isset($_GET['red_id']) ? $row['id_contract'] : ''; ?>"></td>
+            </tr>
             <tr>
                 <td>ФИО сотрудника</td>
                 <td><input type="text" name="fullname" value="<?= 
 isset($_GET['red_id']) ? $row['fullname'] : ''; ?>"></td>
             </tr>
-            <tr>
-                <td>Код сотрудника</td>
-                <td><input type="text" name="id_employee" value="<?= 
-isset($_GET['red_id']) ? $row['id_employee'] : ''; ?>"></td>
-            </tr>
+
             <tr>
                 <td>Дата рождения</td>
                 <td><input type="text" name="date_of_birthday" value="<?= 
