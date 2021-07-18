d91c92d4f983c0cae018f9482c3ca9791832b450
leonovagela <angela-2-0-0-2@mail.ru>
2021-07-18 16:41:52 +0300

    просмотр таблицы сотрудники

---
+++ index_employee_user.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title> Просмотр </title>
+</head>
+
+<body>
+    <h2> Таблица "Сотрудники" </h2>
+    <table border=1>
+        <tr>
+            <td> Код сотрудника </td>
+            <td> ФИО сотрудника </td>
+            <td> Дата рождения сотрудника </td>
+            <td> Должность </td>
+
+        </tr>
+        <?php 
+     $sql_state = "SELECT employee.fullname, employee.id_employee, employee.date_of_birthday, employee.id_post, post.id_post, post.post, post.zp FROM employee INNER JOIN post ON employee.id_post=post.id_post;";
+$result_state = mysqli_query($link, $sql_state);
+while ($row_state = 
+mysqli_fetch_array($result_state)) { 
+ echo '<tr>' .
+ "<td> {$row_state['id_employee']}</td>".
+"<td> {$row_state['fullname']} </td>" .
+
+"<td> {$row_state['date_of_birthday']}</td>".
+"<td> {$row_state['post']}</td>".
+
+'</tr>'; 
+ } 
+ ?>
+    </table>
+    <form action="user1.php" method="post">
+        <input type="submit" value="Вернуться назад">
+    </form>
+</body>
+
+</html>
---
+++ user1.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Подключение БД к provider</title>
+</head>
+<body>
+<form action="index_employee_user.php" method="post">
+        <input type="submit" name="connect" value="сотрудники">
+    </form>
+    <form action="index_post_user.php" method="post">
+        <input type="submit" name="connect" value="должность">
+    </form>
+    <form action="index_department_user.php" method="post">
+        <input type="submit" name="connect" value="отдел">
+    </form>
+    <form action="index_contract_user.php" method="post">
+        <input type="submit" name="connect" value="договор">
+    </form>
+    
+</body>
+</html>
